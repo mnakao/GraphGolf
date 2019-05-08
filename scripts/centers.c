95b30c7c191f57b854d07ca56e68ee17dc39c28e
Masahiro Nakao <masahiro.nakao@riken.jp>
2019-05-08 12:50:45 +0900

    minor fix

--- scripts/centers.c
+++ scripts/centers.c
@@ -2,15 +2,15 @@
 
 int main()
 {
-    int n = 3019, d = 30;
-    //     int n = 4855, d = 30;
-  #define N 7
-    int b_g[N] = {2, 3, 5, 6, 10, 15, 30};
+  //  int n = 3019, d = 30;
+  //  int n = 4855, d = 30;
+  //  int n=1726, d=30;
+  int n=4856, d=15;
+#define N 3
+  int b_g[N] = {3, 5, 15};
+  //  #define N 7
+  //    int b_g[N] = {2, 3, 5, 6, 10, 15, 30};
   
-  //   int n = 50, d = 4;
-  //#define N 2
-  //   int b_g[N] = {2, 4};
-
   for(int i=0;i<N;i++){
     int g = b_g[i];
     for(int c=1;c<n;c++){
@@ -18,8 +18,11 @@ int main()
       if(((n-c)/g) % 2 != 0) continue;
       if((((n-c)/g) - d/g * c)% 2 != 0) continue;
       if((((n-c)/g) - d/g * c) < 0) continue;
-      printf("g = %d c = %d\n", g, c);
-      
+      int based_nodes = (n-c)/g;
+      //      printf("g = %d c = %d : make K DATA=\"..\/data\/n%dd%d.random.edges\" G=%d V=%d E=%d S=$i\n", g, c, based_nodes, d-1, g, c, d/g);
+      //printf("make K DATA=\"..\\/data\\/n%dd%d.random.edges\" G=%d V=%d E=%d S=$i\n", based_nodes, d-1, g, c, d/g);
+      //      printf("make cygnus DATA=\"..\\/data\\/n%dd%d.random.edges\" G=%d V=%d E=%d N=10000000 T=2034.200008\n", based_nodes, d-1, g, c, d/g);
+      printf("python ./scripts/create_random.py %d %d\n", based_nodes, d-1);
     }
   }
   return 0;
