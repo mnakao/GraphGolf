276db9427b722cb03451c062b30597a2d0bb20ba
Masahiro Nakao <masahiro.nakao@riken.jp>
2019-11-12 16:29:37 +0900

    Add nakano version

---
+++ scripts/nakano/common.c
@@ -0,0 +1,383 @@
+#include "common.h"
+
+void printb(const uint64_t v)
+{
+  uint64_t mask = 0x1ULL << (sizeof(v) * CHAR_BIT - 1);
+  int sum = 0;
+  do{
+    putchar(mask & v ? '1' : '0');
+    sum++;
+    if(sum%8==0) putchar(',');
+  } while (mask >>= 1);
+}
+
+void create_adjacency(const int nodes, const int lines, const int degree,
+                      const int edge[lines][2], int adjacency[nodes][degree])
+{
+  int count[nodes];
+  for(int i=0;i<nodes;i++)
+    count[i] = 0;
+
+  for(int i=0;i<lines;i++){
+    int n1 = edge[i][0];
+    int n2 = edge[i][1];
+    adjacency[n1][count[n1]++] = n2;
+    adjacency[n2][count[n2]++] = n1;
+  }
+}
+
+bool has_duplicated_vertex(const int e00, const int e01, const int e10, const int e11)
+{
+  return (e00 == e10 || e01 == e11 || e00 == e11 || e01 == e10);
+}
+
+bool has_duplicated_edge(const int e00, const int e01, const int e10, const int e11)
+{
+  return (e00 == e10 && e01 == e11) || (e00 == e11 && e01 == e10);
+}
+
+int getRandom(const int max)
+{
+  return (int)(random()*((double)max)/(1.0+RAND_MAX));
+}
+
+int WIDTH(const int v, const int height)
+{
+  return v/height;
+}
+
+int HEIGHT(const int v, const int height)
+{
+  return v%height;
+}
+
+int ROTATE(const int v, const int height, const int width,
+	   const int groups, const int degree)
+{
+  if(groups != 2 && groups != 4)
+    ERROR("Invalid groups\n");
+
+  int w = WIDTH (v, height);
+  int h = HEIGHT(v, height);
+  if(groups == 2){
+    if(degree != 180)
+       ERROR("Invalid degree\n");
+    
+    // degree == 180
+    return (width-w-1)*height + (height-h-1);
+  }
+  else{ // groups == 4
+    if(degree != 90 && degree != 180 && degree != 270)
+      ERROR("Invalid degree\n");
+    
+    if(degree == 90)       return h*height + (height-w-1);
+    else if(degree == 180) return (height-w-1)*height + (height-h-1);
+    else                   return (height-h-1)*height + w; // degree == 270
+  }
+}
+
+bool check_degree(const int nodes, const int lines, int edge[lines][2])
+{
+  int n[nodes];
+  for(int i=0;i<nodes;i++)
+    n[i] = 0;
+  
+  for(int i=0;i<lines;i++){
+    n[edge[i][0]]++;
+    n[edge[i][1]]++;
+  }
+  
+  int degree = 2 * lines / nodes;
+  for(int i=0;i<nodes;i++)
+    if(degree != n[i])
+      return false;
+
+  return true;
+}
+
+/*
+// diameter is not implemented
+bool check_vector(const int groups, const int lines, const int height, const int edge[lines][2])
+{
+  if(groups == 1) return true;
+  
+  int based_lines = lines/groups;
+  int vec_w[lines], vec_h[lines];
+  for(int i=0;i<lines;i++){
+    vec_w[i] = WIDTH (edge[i][0], height) - WIDTH (edge[i][1], height);
+    vec_h[i] = HEIGHT(edge[i][0], height) - HEIGHT(edge[i][1], height);
+  }
+
+  if(groups == 2){
+    for(int i=0;i<based_lines;i++)
+      if(!(vec_w[i] == -1 * vec_w[based_lines+i] && vec_h[i] == -1 * vec_h[based_lines+i]))
+	return false;
+  }
+  else{ // groups == 4
+    for(int i=0;i<based_lines;i++){
+      if(!(vec_w[i] == -1 * vec_h[based_lines+i] && vec_h[i] == vec_w[based_lines+i])){
+	printf("%d,%d %d,%d\n", WIDTH (edge[i][0], height),HEIGHT(edge[i][0], height),WIDTH (edge[i][1], height),HEIGHT(edge[i][1], height));
+	printf("%d,%d %d,%d\n", WIDTH (edge[based_lines+i][0], height),HEIGHT(edge[based_lines+i][0], height),
+	       WIDTH (edge[based_lines+i][1], height),HEIGHT(edge[based_lines+i][1], height));
+	printf("A\n");
+        return false;
+      }
+      if(!(vec_w[i] == -1 * vec_w[based_lines*2+i] && vec_h[i] == -1 * vec_h[based_lines*2+i])){
+	printf("%d,%d %d,%d\n", WIDTH (edge[i][0], height),HEIGHT(edge[i][0], height),WIDTH (edge[i][1], height),HEIGHT(edge[i][1], height));
+	printf("%d,%d %d,%d\n", WIDTH (edge[based_lines*2+i][0], height),HEIGHT(edge[based_lines*2+i][0], height),
+               WIDTH (edge[based_lines*2+i][1], height),HEIGHT(edge[based_lines*2+i][1], height));
+	printf("B\n");
+	return false;
+      }
+      if(!(vec_w[i] == vec_h[based_lines*3+i] && vec_h[i] == -1 * vec_w[based_lines*3+i])){
+	printf("%d %d : %d %d\n", vec_w[i], vec_w[based_lines+i], vec_h[i], vec_h[based_lines+i]);
+	printf("C\n");
+        return false;
+      }
+    }
+  }
+  
+  return true;
+}
+*/
+
+bool check_symmetric_edge(const int lines, const int edge[lines][2], const int height,
+			  const int width, const int based_height, const int groups)
+{
+  assert(lines%groups == 0);
+  int tmp_edge[2], based_lines = lines / groups;
+
+  if(groups == 2){
+    for(int i=0;i<based_lines;i++){
+      for(int j=0;j<2;j++)
+	tmp_edge[j] = ROTATE(edge[i][j], height, width, groups, 180);
+      
+      if(!has_duplicated_edge(edge[based_lines+i][0], edge[based_lines+i][1], tmp_edge[0], tmp_edge[1]))
+	if(!( WIDTH (edge[based_lines+i][0], height) + WIDTH (edge[based_lines+i][1], height) == (width-1) &&
+              HEIGHT(edge[based_lines+i][0], height) + HEIGHT(edge[based_lines+i][1], height) == (height-1))){
+	  printf("i=%d: %d,%d-%d,%d %d,%d-%d,%d\n", i,
+                 WIDTH(edge[based_lines+i][0], height), HEIGHT(edge[based_lines+i][0], height),
+                 WIDTH(edge[based_lines+i][1], height), HEIGHT(edge[based_lines+i][1], height),
+                 WIDTH(tmp_edge[0], height), HEIGHT(tmp_edge[0], height),
+                 WIDTH(tmp_edge[1], height), HEIGHT(tmp_edge[1], height));
+	  return false;
+	}
+    }
+  }
+  else if(groups == 4){
+    // 90 degrees
+    for(int i=0;i<based_lines;i++){
+      for(int j=0;j<2;j++)
+	tmp_edge[j] = ROTATE(edge[i][j], height, width, groups, 90);
+      if(!has_duplicated_edge(tmp_edge[0], tmp_edge[1], edge[based_lines+i][0], edge[based_lines+i][1])){
+	if(!( WIDTH (edge[based_lines+i][0], height) + WIDTH (edge[based_lines+i][1], height) == (width-1) &&
+	      HEIGHT(edge[based_lines+i][0], height) + HEIGHT(edge[based_lines+i][1], height) == (height-1))){
+	  printf("A i=%d: %d,%d-%d,%d %d,%d-%d,%d\n", i,
+		 WIDTH(edge[based_lines+i][0], height), HEIGHT(edge[based_lines+i][0], height),
+		 WIDTH(edge[based_lines+i][1], height), HEIGHT(edge[based_lines+i][1], height),
+		 WIDTH(tmp_edge[0], height), HEIGHT(tmp_edge[0], height),
+		 WIDTH(tmp_edge[1], height), HEIGHT(tmp_edge[1], height));
+	  return false;
+	}
+      }
+
+      // 180 degrees
+      for(int j=0;j<2;j++)
+	tmp_edge[j] = ROTATE(edge[i][j], height, width, groups, 180);
+      if(!has_duplicated_edge(tmp_edge[0], tmp_edge[1], edge[based_lines*2+i][0], edge[based_lines*2+i][1])){
+	if(!( WIDTH (edge[based_lines*2+i][0], height) + WIDTH (edge[based_lines*2+i][1], height) == (width-1) &&
+	      HEIGHT(edge[based_lines*2+i][0], height) + HEIGHT(edge[based_lines*2+i][1], height) == (height-1))){
+	  printf("B i=%d: %d,%d-%d,%d %d,%d-%d,%d\n", i, 
+		 WIDTH(edge[based_lines*2+i][0], height), HEIGHT(edge[based_lines*2+i][0], height),
+		 WIDTH(edge[based_lines*2+i][1], height), HEIGHT(edge[based_lines*2+i][1], height),
+		 WIDTH(tmp_edge[0], height), HEIGHT(tmp_edge[0], height),
+		 WIDTH(tmp_edge[1], height), HEIGHT(tmp_edge[1], height));
+	  return false;
+	}
+      }
+
+      // 270 degrees
+      for(int j=0;j<2;j++)
+	tmp_edge[j] = ROTATE(edge[i][j], height, width, groups, 270);
+      if(!has_duplicated_edge(tmp_edge[0], tmp_edge[1], edge[based_lines*3+i][0], edge[based_lines*3+i][1])){
+	if(!( WIDTH (edge[based_lines*3+i][0], height) + WIDTH (edge[based_lines*3+i][1], height) == (width-1) &&
+	      HEIGHT(edge[based_lines*3+i][0], height) + HEIGHT(edge[based_lines*3+i][1], height) == (height-1))){
+	  printf("C i=%d: %d,%d-%d,%d %d,%d-%d,%d\n", i,
+		 WIDTH(edge[based_lines*3+i][0], height), HEIGHT(edge[based_lines*3+i][0], height),
+		 WIDTH(edge[based_lines*3+i][1], height), HEIGHT(edge[based_lines*3+i][1], height),
+		 WIDTH(tmp_edge[0], height), HEIGHT(tmp_edge[0], height),
+		 WIDTH(tmp_edge[1], height), HEIGHT(tmp_edge[1], height));
+	  return false;
+	}
+      }
+    }
+  }
+  
+  return true;
+}
+
+void output_edge(const int lines, const int edge[lines][2], const int height)
+{
+  for(int i=0;i<lines;i++)
+    printf("%d,%d %d,%d\n", WIDTH(edge[i][0], height), HEIGHT(edge[i][0], height),
+	   WIDTH(edge[i][1], height), HEIGHT(edge[i][1], height));
+}
+
+void copy_edge(int *restrict buf1, const int *restrict buf2, const int n)
+{
+#ifdef _OPENMP
+#pragma omp parallel for
+#endif
+  for(int i=0;i<n;i++)
+    buf1[i] = buf2[i];
+}
+
+void swap(int *a, int *b)
+{
+  int tmp = *a;
+  *a = *b;
+  *b = tmp;
+}
+
+bool check_loop(const int lines, const int edge[lines][2])
+{
+  timer_start(TIMER_CHECK);
+  bool flag = true;
+
+#ifdef _OPENMP
+#pragma omp parallel for
+#endif
+  for(int i=0;i<lines;i++)
+    if(edge[i][0] == edge[i][1])
+      flag = false;
+
+  timer_stop(TIMER_CHECK);
+
+  if(flag == false){
+    for(int i=0;i<lines;i++)
+      if(edge[i][0] == edge[i][1]){
+	printf("%d: %d %d <--\n", i, edge[i][0], edge[i][1]);
+      }
+      else{
+	printf("%d: %d %d\n", i, edge[i][0], edge[i][1]);
+      }
+  }
+  
+  return flag;
+}
+
+bool check_duplicate_all_edge(const int lines, const int edge[lines][2])
+{
+  timer_start(TIMER_CHECK);
+  bool flag = true;
+
+#ifdef _OPENMP
+#pragma omp parallel for
+#endif
+  for(int i=0;i<lines;i++)
+    for(int j=i+1;j<lines;j++)
+      if(has_duplicated_edge(edge[i][0], edge[i][1], edge[j][0], edge[j][1])){
+	printf("%d %d %d %d\n", edge[i][0], edge[i][1], edge[j][0], edge[j][1]);
+        flag = false;
+      }
+
+  timer_stop(TIMER_CHECK);
+  return flag;
+}
+
+bool check_duplicate_tmp_edge(const int g_opt, const int groups, int tmp_edge[groups*g_opt][2])
+{
+  timer_start(TIMER_CHECK);
+  bool flag = true;
+
+  for(int i=0;i<g_opt;i++){
+    int tmp[2] = {tmp_edge[i][0], tmp_edge[i][1]};
+    for(int j=g_opt;j<groups*g_opt;j++)
+      if(has_duplicated_edge(tmp[0], tmp[1], tmp_edge[j][0], tmp_edge[j][1]))
+	flag = false;
+  }
+
+  timer_stop(TIMER_CHECK);
+  return flag;
+}
+
+bool check_duplicate_current_edge(const int lines, const int edge[lines][2], const int tmp_lines,
+				  const int tmp_edge[tmp_lines][2], const int tmp_line[2],
+				  const int groups, const int g_opt, const bool is_center)
+{
+  timer_start(TIMER_CHECK);
+  int based_lines = lines/groups;
+  bool flag = true;
+
+  if(g_opt == D_2G_OPT){
+    int tmp_line0 = tmp_line[0]%based_lines;
+    int tmp_line1 = tmp_line[1]%based_lines;
+
+#ifdef _OPENMP
+#pragma omp parallel for
+#endif
+    for(int i=rank;i<based_lines;i+=procs)
+      if(i != tmp_line0 && i != tmp_line1)
+        for(int j=0;j<tmp_lines;j++)
+          if(has_duplicated_edge(edge[i][0], edge[i][1], tmp_edge[j][0], tmp_edge[j][1]))
+            flag = false;
+  }
+  else if(g_opt == D_1G_OPT){
+    int tmp_line0 = tmp_line[0]%based_lines;
+    if(! is_center){
+#ifdef _OPENMP
+#pragma omp parallel for
+#endif
+      for(int i=rank;i<based_lines;i+=procs)
+	if(i != tmp_line0)
+	  for(int j=0;j<tmp_lines;j++)
+	    if(has_duplicated_edge(edge[i][0], edge[i][1], tmp_edge[j][0], tmp_edge[j][1]))
+	      flag = false;
+    }
+    else{
+#ifdef _OPENMP
+#pragma omp parallel for
+#endif
+      for(int i=rank;i<lines;i+=procs)
+        if(i%based_lines != tmp_line0)
+          for(int j=0;j<tmp_lines;j++)
+            if(has_duplicated_edge(edge[i][0], edge[i][1], tmp_edge[j][0], tmp_edge[j][1]))
+              flag = false;
+    }
+  }
+
+  MPI_Allreduce(MPI_IN_PLACE, &flag, 1,  MPI_C_BOOL, MPI_LAND, MPI_COMM_WORLD);   
+  timer_stop(TIMER_CHECK);
+  return flag;
+}
+
+void create_rotate_hash(const int nodes, const int height, const int width, const int groups,
+			int *rotate_hash)
+{
+  int based_nodes = nodes / groups;
+  
+  if(groups == 1){
+    for(int i=0;i<based_nodes;i++)
+      rotate_hash[i] = i;
+  }
+  else if(groups == 2){
+    int based_height = height / 2;
+
+    for(int i=0;i<based_nodes;i++){
+      int j = (i/based_height) * height + (i%based_height);
+      rotate_hash[j] = i;
+      rotate_hash[ROTATE(j, height, width, groups, 180)] = i + based_nodes;
+    }
+  }
+  else{
+    int based_height = height / 2;
+    
+    for(int i=0;i<based_nodes;i++){
+      int j = (i/based_height) * height + (i%based_height);
+      rotate_hash[j] = i;
+      rotate_hash[ROTATE(j, height, width, groups,  90)] = i + based_nodes;
+      rotate_hash[ROTATE(j, height, width, groups, 180)] = i + based_nodes * 2;
+      rotate_hash[ROTATE(j, height, width, groups, 270)] = i + based_nodes * 3;
+    }
+  }
+}
---
+++ scripts/nakano/sa.c
@@ -0,0 +1,332 @@
+#include "common.h"
+
+static double uniform_rand()
+{
+  return ((double)random()+1.0)/((double)RAND_MAX+2.0);
+}
+
+static void print_result_header()
+{
+  PRINT_R0("   Times\tTemp\t\tCur. ASPL GAP\t\tBest ASPL GAP              ");
+  PRINT_R0("Cur. Dia. GAP    Best Dia. GAP    Cur. Len. GAP    Best Len. GAP    Accept Rate\n");
+}
+
+static void print_results(const long long num, const double temp, 
+			  const double current_ASPL, const double best_ASPL, const double low_ASPL,
+			  const int current_diam,    const int best_diam,    const int low_diam,
+			  const int current_length,  const int best_length,  const int low_length,
+			  const bool enable_fixed_temp, const double fixed_temp,
+			  const long long accepts, const long long rejects)
+{
+  if(enable_fixed_temp)
+    PRINT_R0("%8lld\t%f\t", num, fixed_temp);
+  else
+    PRINT_R0("%8lld\t%f\t", num, temp);
+  PRINT_R0("%f ( %f )   %f ( %f )     %2d ( %2d )        %2d ( %2d )        %2d ( %2d )        %2d ( %2d )         ",
+	   current_ASPL,   current_ASPL-low_ASPL,     best_ASPL,   best_ASPL-low_ASPL,
+	   current_diam,   current_diam-low_diam,     best_diam,   best_diam-low_diam,
+	   current_length, current_length-low_length, best_length, best_length-low_length);
+  if(num != 0)
+    PRINT_R0("%.4f ( %lld / %lld )\n", (double)accepts/(accepts+rejects), accepts, (accepts+rejects));
+  else
+    PRINT_R0("-\n");
+}  
+
+static void exchange_edge(const int nodes, const int lines, const int degree, int edge[lines][2],
+			  const int height, const int width, const int groups, const int low_length, 
+			  const double max_temp, const double min_temp, const double temp, const long long ii)
+{
+  int tmp_line[groups*2], based_lines = lines/groups;
+  assert(lines%groups == 0);
+  assert(nodes%groups == 0);
+
+  while(1){
+    while(1){
+      tmp_line[0] = getRandom(lines);
+      tmp_line[1] = getRandom(lines);
+      if(tmp_line[0] == tmp_line[1]) continue;
+      else if(has_duplicated_vertex(edge[tmp_line[0]][0], edge[tmp_line[0]][1], edge[tmp_line[1]][0], edge[tmp_line[1]][1])){
+        continue;
+      }
+      else if((tmp_line[0] - tmp_line[1]) % based_lines == 0){
+	ERROR("1g-opt\n");
+      }
+      else
+	break;
+    }
+
+    for(int i=1;i<groups;i++){
+      tmp_line[i*2  ] = tmp_line[0] + based_lines * i;
+      tmp_line[i*2+1] = tmp_line[1] + based_lines * i;
+      if(tmp_line[i*2  ] >= lines) tmp_line[i*2  ] -= lines;
+      if(tmp_line[i*2+1] >= lines) tmp_line[i*2+1] -= lines;
+    }
+
+#ifdef _DEBUG_MSG
+    printf("edge_2g_opt: ii = %lld\n", ii);
+    for(int i=0;i<groups*2;i++)
+      printf("line[%d] = %d\n", i, tmp_line[i]);
+
+    for(int i=0;i<groups;i++)
+      printf("Before: %d,%d-%d,%d %d,%d-%d,%d\n",
+    	     WIDTH(edge[tmp_line[i*2  ]][0], height), HEIGHT(edge[tmp_line[i*2  ]][0], height),
+    	     WIDTH(edge[tmp_line[i*2  ]][1], height), HEIGHT(edge[tmp_line[i*2  ]][1], height),
+    	     WIDTH(edge[tmp_line[i*2+1]][0], height), HEIGHT(edge[tmp_line[i*2+1]][0], height),
+	     WIDTH(edge[tmp_line[i*2+1]][1], height), HEIGHT(edge[tmp_line[i*2+1]][1], height));
+#endif
+    
+    int r = getRandom(2);
+    int tmp_edge[groups*2][2];
+    for(int i=0;i<groups;i++){
+      for(int j=0;j<2;j++){
+	tmp_edge[i*2  ][j] = edge[tmp_line[i*2  ]][j];
+	tmp_edge[i*2+1][j] = edge[tmp_line[i*2+1]][j];
+      }
+      swap(&tmp_edge[i*2][1], &tmp_edge[i*2+1][r]);
+    }
+
+    bool flag = false;
+    for(int i=0;i<2;i++){
+      int w0 = WIDTH (tmp_edge[i][0], height);
+      int h0 = HEIGHT(tmp_edge[i][0], height);
+      int w1 = WIDTH (tmp_edge[i][1], height);
+      int h1 = HEIGHT(tmp_edge[i][1], height);
+      int tmp_length = abs(w0 - w1) + abs(h0 - h1);
+      if(tmp_length > low_length)
+	flag = true;
+    }
+    if(flag) continue;
+    
+    if(!check_duplicate_tmp_edge(D_2G_OPT, groups, tmp_edge))
+      continue;
+    else if(!check_duplicate_current_edge(lines, (const int (*)[2])edge, groups*2, 
+					  (const int (*)[2])tmp_edge, tmp_line, groups, D_2G_OPT, false))
+      continue;
+
+    for(int i=0;i<groups;i++){
+      for(int j=0;j<2;j++){
+	edge[tmp_line[i*2  ]][j] = tmp_edge[i*2  ][j];
+	edge[tmp_line[i*2+1]][j] = tmp_edge[i*2+1][j];
+      }
+
+#ifdef _DEBUG_MSG
+       printf("After : %d,%d-%d,%d %d,%d-%d,%d\n",
+	      WIDTH(edge[tmp_line[i*2  ]][0], height), HEIGHT(edge[tmp_line[i*2  ]][0], height),
+	      WIDTH(edge[tmp_line[i*2  ]][1], height), HEIGHT(edge[tmp_line[i*2  ]][1], height),
+	      WIDTH(edge[tmp_line[i*2+1]][0], height), HEIGHT(edge[tmp_line[i*2+1]][0], height),
+	      WIDTH(edge[tmp_line[i*2+1]][1], height), HEIGHT(edge[tmp_line[i*2+1]][1], height));
+#endif
+     }
+    break;
+  }
+}
+
+// When the diameter is small, the length becomes long, so the diameter isn't adopted for evaluation.
+static bool accept(const double new_ASPL, const double current_ASPL, const bool enable_fixed_temp, const double fixed_temp,
+		   const int new_total_over_length, const int current_total_over_length, const double temp,
+		   const int nodes, const int degree, const bool enable_hill_climbing, const bool enable_detect_temp, 
+		   double *max_diff_energy, long long *total_accepts, long long *accepts, long long *rejects,
+		   const double max_temp, const double min_temp, const int groups, const long long ii)
+{
+  double diff = ((current_ASPL-new_ASPL)*nodes*(nodes-1)) / groups;
+
+  if(enable_detect_temp){
+    *max_diff_energy = MAX(*max_diff_energy, -1.0 * diff);
+  }
+  else{
+    if(diff >= 0){
+      *accepts += 1;
+      if(ii > SKIP_ACCEPTS) *total_accepts +=1;
+      return true;
+    }
+    if(enable_hill_climbing){ // Only accept when new_ASPL <= current_ASPL.
+      *rejects += 1;
+      return false;
+    }
+  }
+
+  double v = (enable_fixed_temp)? exp(diff/fixed_temp) : exp(diff/temp);
+
+  if(v > uniform_rand()){
+    *accepts += 1;
+    if(ii > SKIP_ACCEPTS) *total_accepts +=1;
+    return true;
+  }
+  else{
+    *rejects += 1;
+    return false;
+  }
+}
+
+static void calc_length(const int lines, int edge[lines][2], const int height,
+			const int low_length, int *length, int *total_over_length)
+{
+  *length = 0;
+  *total_over_length = 0;
+  
+  for(int i=0;i<lines;i++){
+    int w0 = WIDTH (edge[i][0], height);
+    int h0 = HEIGHT(edge[i][0], height);
+    int w1 = WIDTH (edge[i][1], height);
+    int h1 = HEIGHT(edge[i][1], height);
+    int distance = abs(w0 - w1) + abs(h0 - h1);
+    *length = MAX((*length), distance);
+    if(distance > low_length)
+      *total_over_length += (distance-low_length);
+  }
+}
+
+long long sa(const int nodes, const int lines, double temp, const long long ncalcs,
+	     const double cooling_rate, const int low_diam,  const double low_ASPL, const bool enable_bfs, 
+	     const bool enable_hill_climbing, const bool enable_detect_temp,
+	     double *max_diff_energy, const double max_temp, const double min_temp,
+	     const bool enable_fixed_temp,  const double fixed_temp, int edge[lines][2],
+	     int *diam, double *ASPL, const int cooling_cycle, long long *total_accepts, const int width,
+	     const int based_width, const int height, const int based_height, int *length,
+	     const int low_length, const int groups,  const int *rotate_hash)
+{
+  int degree = 2*lines/nodes, best_edge[lines][2], tmp_edge[lines][2], based_nodes = nodes/groups;
+  long long ii, accepts = 0, rejects = 0;
+
+  // Create adjacency matrix
+  int (*adjacency)[degree] = malloc(sizeof(int)*nodes*degree); // int adjacency[nodes][degree];
+  create_adjacency(nodes, lines, degree, (const int (*)[2])edge, adjacency);
+  evaluation(nodes, degree, groups, (const int* restrict)adjacency,
+	     based_nodes, height, based_height, diam, ASPL, enable_bfs, rotate_hash);
+
+  int tmp_total_over_length = 0;
+  calc_length(lines, edge, height, low_length, length, &tmp_total_over_length);
+
+  double current_ASPL = *ASPL,   best_ASPL   = *ASPL,   tmp_ASPL;
+  int current_diam    = *diam,   best_diam   = *diam,   tmp_diam;
+  int current_length  = *length, best_length = *length, tmp_length;
+  int current_total_over_length = tmp_total_over_length;
+  int print_interval  = (ncalcs/NUM_OF_PROGRESS == 0)? 1 : ncalcs/NUM_OF_PROGRESS;
+  copy_edge((int *)best_edge, (int *)edge, lines*2);
+    
+  if(rank == 0 && !enable_detect_temp)
+    print_result_header();
+
+  for(ii=0;ii<ncalcs;ii++){
+    if(ii % print_interval == 0 && !enable_detect_temp){
+      print_results(ii, temp,
+		    current_ASPL,   best_ASPL,   low_ASPL,
+		    current_diam,   best_diam,   low_diam,
+		    current_length, best_length, low_length,
+		    enable_fixed_temp, fixed_temp,
+		    accepts, rejects);
+      accepts = 0;
+      rejects = 0;
+    }
+
+    while(1){
+      copy_edge((int *)tmp_edge, (int *)edge, lines*2);
+      exchange_edge(nodes, lines, degree, tmp_edge, height, width, groups, low_length,
+		    max_temp, min_temp, temp, ii);
+      assert(check_loop(lines, tmp_edge));
+      assert(check_duplicate_all_edge(lines, tmp_edge));
+      assert(check_degree(nodes, lines, tmp_edge));
+      assert(check_symmetric_edge(lines, tmp_edge, height, width, based_height, groups));
+      create_adjacency(nodes, lines, degree, (const int (*)[2])tmp_edge, adjacency);
+      if(evaluation(nodes, degree, groups, (const int* restrict)adjacency,
+		    based_nodes, height, based_height, &tmp_diam, &tmp_ASPL, enable_bfs, rotate_hash)){
+	calc_length(lines, tmp_edge, height, low_length, &tmp_length, &tmp_total_over_length);
+	break;
+      }
+    }
+
+    if(accept(tmp_ASPL, current_ASPL, enable_fixed_temp, fixed_temp,
+	      tmp_total_over_length, current_total_over_length,
+	      temp, nodes, degree, enable_hill_climbing, enable_detect_temp,
+	      max_diff_energy, total_accepts, &accepts, &rejects,
+	      max_temp, min_temp, groups, ii)){
+      current_ASPL   = tmp_ASPL;
+      current_diam   = tmp_diam;
+      current_length = tmp_length;
+      current_total_over_length = tmp_total_over_length;
+      copy_edge((int *)edge, (int *)tmp_edge, lines*2);
+      if((low_diam <= tmp_diam) &&
+	 ((best_length > tmp_length) ||
+	  (best_length == tmp_length && best_diam > tmp_diam) ||
+	  (best_length == tmp_length && best_diam == tmp_diam && best_ASPL > tmp_ASPL))){
+	copy_edge((int *)best_edge, (int *)edge, lines*2);
+	best_length = tmp_length;
+	best_diam   = tmp_diam;
+	best_ASPL   = tmp_ASPL;
+      }
+
+      if(best_ASPL == low_ASPL && best_length == low_length){
+	if(!enable_detect_temp){
+	  print_results(ii, temp, current_ASPL, best_ASPL, low_ASPL,
+			current_diam, best_diam, low_diam,
+			current_length, best_length, low_length,
+			enable_fixed_temp, fixed_temp,
+			accepts, rejects);
+	  PRINT_R0("---\nFound optimum solution.\n");
+	}
+	break;
+      }
+    }
+    
+    if((ii+1)%cooling_cycle == 0)
+      temp *= cooling_rate;
+  }
+
+  *ASPL   = best_ASPL;
+  *diam   = best_diam;
+  *length = best_length;
+  copy_edge((int *)edge, (int *)best_edge, lines*2);
+  free(adjacency);
+
+  return ii;
+}
+
+double estimated_elapse_time(const int nodes, const int lines, const int edge[lines][2],
+			     const int height, const int width, const int based_height, const int groups,
+			     const int low_length, const bool enable_bfs, const int *rotate_hash)
+{
+  int based_nodes = nodes / groups;
+  int degree = 2 * lines / nodes;
+  int diam;    // Not use
+  double ASPL; // Not use
+  int (*adjacency)[degree] = malloc(sizeof(int)*nodes*degree); // int adjacency[nodes][degree];
+  int (*tmp_edge)[2]       = malloc(sizeof(int)*lines*2);      // int tmp_edge[lines][2];
+  
+  timer_start(TIMER_ESTIMATED);
+  for(int i=0;i<ESTIMATED_TIMES;i++){
+    copy_edge((int *)tmp_edge, (int *)edge, lines*2);
+    exchange_edge(nodes, lines, degree, tmp_edge, height, width, groups, low_length,
+		  (double)NOT_USED, (double)NOT_USED, (double)NOT_USED, (int)i);
+    create_adjacency(nodes, lines, degree, (const int (*)[2])tmp_edge, adjacency);
+    evaluation(nodes, degree, groups, (const int* restrict)adjacency,
+	       based_nodes, height, based_height, &diam, &ASPL, enable_bfs, rotate_hash);
+  }
+  timer_stop(TIMER_ESTIMATED);
+  
+  free(tmp_edge);
+  free(adjacency);
+
+  return timer_read(TIMER_ESTIMATED)/ESTIMATED_TIMES;
+}
+
+// This function is mainly useful when groupe is 1.
+void check_current_edge(const int nodes, const int lines, int edge[lines][2], const double low_ASPL,
+			const int groups, const int height, const int based_height, const bool enable_bfs,
+			const int *rotate_hash)
+{
+  int based_nodes = nodes / groups;
+  int degree = 2 * lines / nodes;
+  int diam;    // Not use
+  double ASPL;
+  int (*adjacency)[degree] = malloc(sizeof(int)*nodes*degree); // int adjacency[nodes][degree];
+
+  create_adjacency(nodes, lines, degree, (const int (*)[2])edge, adjacency);
+  if(! evaluation(nodes, degree, groups, (const int* restrict)adjacency,
+		  based_nodes, height, based_height, &diam, &ASPL, enable_bfs, rotate_hash))
+    ERROR("The input file has a node which is never reached by another node.\n");
+
+  if(ASPL == low_ASPL)
+    END("The input file has already optimum solution.\n");
+
+  free(adjacency);
+}
