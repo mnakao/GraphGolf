dcc99ae96b62b833a027ed135895fc3a4add4f1c
Masahiro Nakao <masahiro.nakao@riken.jp>
2020-02-20 17:57:22 +0900

    minor fix

--- scripts/createKaryNcube/main.c
+++ scripts/createKaryNcube/main.c
@@ -1,68 +1,70 @@
 #include <stdio.h>
 #include <math.h>
 #include <stdlib.h>
-#define DIMS 2
-#define N    16
+#define DIMS 3
+#define N1   8
+#define N2   8
+#define N3   4
 
 int main()
 {
   int lines = 0, d[DIMS];
 
 #if DIMS == 2
-  fprintf(stderr, "%d x %d, degree = %d\n", N, N, 4);
+  fprintf(stderr, "%d x %d, degree = %d\n", N2, N1, 4);
   
-  int n[N][N], edge[(N*N*4)/2][2];
-  for(int j=0;j<N;j++)
-    for(int i=0;i<N;i++)
-      n[j][i] = j * N + i;
+  int n[N2][N1], edge[(N2*N1*4)/2][2];
+  for(int j=0;j<N2;j++)
+    for(int i=0;i<N1;i++)
+      n[j][i] = j * N1 + i;
 
-  for(int j=0;j<N;j++){
-    for(int i=0;i<N;i++){
+  for(int j=0;j<N2;j++){
+    for(int i=0;i<N1;i++){
       int c = n[j][i];
-      d[1] = c / N;
-      d[0] = c % N;
-      if(d[0] != N - 1){ // Add right line
+      d[1] = c / N1;
+      d[0] = c % N1;
+      if(d[0] != N1 - 1){ // Add right line
       	edge[lines][0] = c;
       	edge[lines][1] = c + 1;
       	lines++;
       }
-      if(d[1] != N - 1){ // Add down line
+      if(d[1] != N2 - 1){ // Add down line
 	edge[lines][0] = c;
-	edge[lines][1] = c + N;
+	edge[lines][1] = c + N1;
 	lines++;
       }
     }
   }
 
 #elif DIMS == 3
-  fprintf(stderr, "%d x %d x %d, degree = %d\n", N, N, N, 6);
+  fprintf(stderr, "%d x %d x %d, degree = %d\n", N3, N2, N1, 6);
 
-  int n[N][N][N], edge[(N*N*N*6)/2][2];
-  for(int k=0;k<N;k++)
-    for(int j=0;j<N;j++)
-      for(int i=0;i<N;i++)
-	n[k][j][i] = k * (N * N) + j * N + i;
+  int n[N3][N2][N1], edge[(N3*N2*N1*6)/2][2];
+  for(int k=0;k<N3;k++)
+    for(int j=0;j<N2;j++)
+      for(int i=0;i<N1;i++)
+	n[k][j][i] = k * (N2 * N1) + j * N1 + i;
 
-  for(int k=0;k<N;k++){
-    for(int j=0;j<N;j++){
-      for(int i=0;i<N;i++){
+  for(int k=0;k<N3;k++){
+    for(int j=0;j<N2;j++){
+      for(int i=0;i<N1;i++){
 	int c = n[k][j][i];
-	d[2] = c / (N*N);
-	d[1] = (c - d[2]*(N*N)) / N;
-	d[0] = c % N;
-	if(d[0] != N - 1){
+	d[2] = c / (N2*N1);
+	d[1] = (c - d[2]*(N2*N1)) / N1;
+	d[0] = c % N1;
+	if(d[0] != N1 - 1){
 	  edge[lines][0] = c;
 	  edge[lines][1] = c + 1;
 	  lines++;
 	}
-	if(d[1] != N - 1){
+	if(d[1] != N2 - 1){
 	  edge[lines][0] = c;
-	  edge[lines][1] = c + N;
+	  edge[lines][1] = c + N1;
 	  lines++;
 	}
-	if(d[2] != N - 1){
+	if(d[2] != N3 - 1){
           edge[lines][0] = c;
-          edge[lines][1] = c + N * N;
+          edge[lines][1] = c + N2 * N1;
           lines++;
         }
       }
